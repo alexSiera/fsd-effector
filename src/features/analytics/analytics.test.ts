a97672b584800f19143fbe72916839667e27b440
alexSiera <fa1nt.siera@gmail.com>
2024-02-16 01:01:27 +0300

    1.14.49: End up doing first analytics test, continue develop analytic service with TDD approach

--- src/features/analytics/analytics.test.ts
+++ src/features/analytics/analytics.test.ts
@@ -1,8 +1,32 @@
-import { empty } from "./analytics";
+import { fork, allSettled } from "effector";
+import { initAnalyticsFx, sendDataToAnalytics, sendEventFx } from "./analytics";
+import { AnalyticsEventTypes } from "../../shared/types/analytics";
 
 describe("analytics", () => {
-  test("should run tests", () => {
-    empty();
-    expect(true).toBe(true);
+  test("should send events immidiatly", async () => {
+    const sendEventMock = jest.fn();
+
+    const TEST_EVENT = { name: AnalyticsEventTypes.FORM_SHOWN, payload: {} };
+
+    const scope = fork({
+      // Список хэндлеров эвентов которые мы хотим подменить в этом скоупе
+      // Пара [mockEffect, mockRealisation]
+      handlers: new Map<any, any>([
+        [initAnalyticsFx, jest.fn()],
+        [sendEventFx, sendEventMock],
+      ]),
+    });
+    // init analytics
+    // Run effect in scope and wait for all triggered effects to settle. This method never throw an error
+    // Если initAnalyticsFx будет вызывавать другие эффекты то эта функция дождется конца всех порожденных ивентов
+    await allSettled(initAnalyticsFx, { scope });
+    // send event
+    await allSettled(sendDataToAnalytics, {
+      scope,
+      params: TEST_EVENT,
+    });
+    // check event sent
+    expect(sendEventMock).toHaveBeenCalledTimes(1);
+    expect(sendEventMock).toHaveBeenCalledWith(TEST_EVENT);
   });
 });
--- src/features/analytics/analytics.ts
+++ src/features/analytics/analytics.ts
@@ -1,1 +1,20 @@
+import { createDomain, sample } from "effector";
+import { AnalyticsEvent } from "../../shared/types/analytics";
+import { initAnalytics, sendEvent } from "./lib";
+
+const analyticsDomain = createDomain();
+
+const initAnalyticsFx = analyticsDomain.createEffect(initAnalytics);
+
+const sendEventFx = analyticsDomain.createEffect(sendEvent);
+
+const sendDataToAnalytics = analyticsDomain.createEvent<AnalyticsEvent>();
+
+sample({
+  source: sendDataToAnalytics,
+  target: sendEventFx,
+});
+
+export { analyticsDomain, sendDataToAnalytics, initAnalyticsFx, sendEventFx };
+
 export function empty() {}
